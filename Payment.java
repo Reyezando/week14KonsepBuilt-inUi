af4b08ebadea2656f16b93672a498bc31906faf8
ReynardoE15 <47804080+ReynardoE15@users.noreply.github.com>
2020-04-21 21:30:12 +0700

    Add files via upload

---
+++ Payment.java
@@ -0,0 +1,44 @@
+package week11;
+
+public abstract class Payment implements ClassInfo {
+	protected boolean isPaidOff;
+	protected Item item;
+	
+	public abstract int pay(int bayar);
+	
+	public Payment() {
+		this.isPaidOff = false;
+		this.item = null;
+	}
+	
+	public Payment (Item item) {
+		this.isPaidOff = false;
+		this.item = item;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	
+	public String getItemName() {
+		return item.getName();
+	}
+	
+	public String getStatus() {
+		if(isPaidOff) {
+			return "FINISHED";
+		}
+		return "ON PROGRESS";
+	}
+	
+	public int getRemainingAmount() {
+		if(isPaidOff) {
+			return 0;
+		}
+		return item.getPrice();
+	}
+	
+	public boolean getPaidOff() {
+		return isPaidOff;
+	}
+}
